e5193e9264b1b4d9d5ab4c452f21878c69859c91
Deepak Sharma <sharma.deepak8616@gmail.com>
2022-10-05 20:50:03 -0400

    Homework 1

---
+++ Modules/Class4/homework-1.js
@@ -0,0 +1,26 @@
+/**
+ * Convert temperature values into different units
+ * 
+ * F -> C
+ * F -> K
+ * 
+ * C -> F
+ * C -> K
+ * 
+ * K -> F
+ * K -> C
+ * 
+ * Refer link for formula:
+ * https://www.rapidtables.com/convert/temperature/fahrenheit-to-celsius.html
+ * 
+ * 
+ * Extra:
+ *      Try to find how to get only 2-digits after the decimal point
+ * 
+ */
+
+// F -> C
+// T(°C) = (T(°F) - 32) / 1.8
+let fTemp1 = 60;
+let cTemp1 = (fTemp1 - 32) / 1.8;
+console.log(`\n${fTemp1}°F is equals to ${cTemp1}°C`);
